b53bdcc29b0b980ef62139d44e843dcfd1a0c147
Paul Agron <paul@cohesion.local>
2019-11-20 13:02:14 -0800

    update

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 var config = {
   position: 'start',
-  draggable: true
+  draggable: true,
+  showNotation: false
+
 }
 
 
 var board1 = ChessBoard('board1', config);
+var board2 = ChessBoard('board2', config);
+var board3 = ChessBoard('board3', config);
+var board4 = ChessBoard('board4', config);
