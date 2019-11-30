67c8c6c58e77fed5b669efcff0105fea3a64e0e3
Paul Agron <paul@cohesion.local>
2019-11-30 01:45:09 -0800

    update

--- script.js
+++ script.js
@@ -48,6 +48,12 @@ class PGNViewer {
 		this.board.start();
 	}
 
+    end() {
+        this.index = this.history.length - 1;
+        this.showAtIndex();
+
+    }
+
 }
 
 
@@ -149,9 +155,32 @@ var pgn = ['1. Nf3 Nf6  2. g3 b6  3. Bg2 Bb7 4. d3 d5  5. O-O e6 6. Nc3 Be7',
 '31. Ng3 Kg8  32. Rc1 Kf8   33. h5 Ke8   34. h6 gxh6  35. Nh5 Kd7 36. Qh4 Ne8',
 '37.Bxh6 Nd8  38.Rxc8 Rxc8  39.g7   1-0'];
 
-var pgn2 = ['1.d4 d5 2.Nc3 Nc6 3.Bg5 Nf6 4.e3 g6 5.Qd2 Bg7 6.O-O-O Bf5 7.h3 Qd6 8.Nf3',
-'O-O-O 9.Bf4 a6 *'];
+var operaGamePGN = `
+1. e4 e5 2. Nf3 d6 3. d4 Bg4 4. dxe5 Bxf3 5. Qxf3 dxe5 6. Bc4 Nf6 
+7. Qb3 Qe7 8. Nc3 c6 9. Bg5 b5 10. Nxb5 cxb5 11. Bxb5+ Nbd7 
+12. O-O-O Rd8 13. Rxd7 Rxd7 14. Rd1 Qe6 15. Bxd7+ Nxd7 16. Qb8+ Nxb8 
+17. Rd8#`
 
+var laskerBauerPGN = `
+1.f4 d5 2.e3 Nf6 3.b3 e6 4.Bb2 Be7 5.Bd3 b6 6.Nc3 Bb7 7.Nf3
+Nbd7 8.O-O O-O 9.Ne2 c5 10.Ng3 Qc7 11.Ne5 Nxe5 12.Bxe5 Qc6
+13.Qe2 a6 14.Nh5 Nxh5 15.Bxh7+ Kxh7 16.Qxh5+ Kg8 17.Bxg7 Kxg7
+18.Qg4+ Kh7 19.Rf3 e5 20.Rh3+ Qh6 21.Rxh6+ Kxh6 22.Qd7 Bf6
+23.Qxb7 Kg7 24.Rf1 Rab8 25.Qd7 Rfd8 26.Qg4+ Kf8 27.fxe5 Bg7
+28.e6 Rb7 29.Qg6 f6 30.Rxf6+ Bxf6 31.Qxf6+ Ke8 32.Qh8+ Ke7
+33.Qg7+ Kxe6 34. Qxb7 Rd6 35. Qxa6 d4 36. exd4 cxd4 37. h4 d3
+38. Qxd3`
+
+var spasskyFischerBenoniPGN = `
+1. d4 Nf6 2. c4 e6 3. Nf3 c5 4. d5 exd5 5. cxd5 d6 6. Nc3 g6
+7. Nd2 Nbd7 8. e4 Bg7 9. Be2 O-O 10. O-O Re8 11. Qc2 Nh5
+12. Bxh5 gxh5 13. Nc4 Ne5 14. Ne3 Qh4 15. Bd2 Ng4 16. Nxg4
+hxg4 17. Bf4 Qf6 18. g3 Bd7 19. a4 b6 20. Rfe1 a6 21. Re2 b5
+22. Rae1 Qg6 23. b3 Re7 24. Qd3 Rb8 25. axb5 axb5 26. b4 c4
+27. Qd2 Rbe8 28. Re3 h5 29. R3e2 Kh7 30. Re3 Kg8 31. R3e2 Bxc3
+32. Qxc3 Rxe4 33. Rxe4 Rxe4 34. Rxe4 Qxe4 35. Bh6 Qg6 36. Bc1
+Qb1 37. Kf1 Bf5 38. Ke2 Qe4+ 39. Qe3 Qc2+ 40. Qd2 Qb3 41. Qd4
+Bd3+`
 
 game.load_pgn(pgn.join('\n'));
 var h = game.history({ verbose: true });
@@ -196,44 +225,32 @@ function cycle(count) {
 
 }
 
-//start cycling through moves on the main board
+//start cycling through moves on the main board recursively
 cycle(0);
 
 
-var text = pgn2.join('\n');
+// var text = pgn2.join('\n');
+
 var controllers = [
-	new PGNViewer(text,board2), 
-	new PGNViewer(text,board3),
-	new PGNViewer(text,board4)];
+	new PGNViewer(operaGamePGN,board2), 
+	new PGNViewer(laskerBauerPGN,board3),
+	new PGNViewer(spasskyFischerBenoniPGN,board4)];
 
 // controlling the little boards
 function start(idx) {
-	switch (idx) {
-		case 2: controllers[0].start(); break;
-		case 3: controllers[1].start(); break;
-		case 4: controllers[2].start(); break;
-	}
-	
+    controllers[idx].start();
 }
 
 function prev(idx) {
-    switch (idx) {
-        case 2: controllers[0].prev(); break;
-        case 3: controllers[1].prev(); break;
-        case 4: controllers[2].prev(); break;
-    }
+    controllers[idx].prev();
 }
 
 function next(idx) {
-	switch (idx) {
-		case 2: controllers[0].next(); break;
-		case 3: controllers[1].next(); break;
-		case 4: controllers[2].next(); break;
-	}
+    controllers[idx].next();
 }
 
 function end(idx) {
-	console.log("end" + idx);
+    controllers[idx].end();
 }
 
 
